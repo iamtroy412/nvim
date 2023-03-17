b14bddd7b252624e9885df327050e7e48504cb6e
Troy Caro <iamtroy412@gmail.com>
2023-03-17 10:59:26 -0400

    Add more key remappings

--- lua/troy/remap.lua
+++ lua/troy/remap.lua
@@ -1,2 +1,15 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Move up/down while in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Half page jumping with up/down and keep cursor in the middle
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+-- next greatest remap ever : asbjornHaland
+-- yank to system keyboard with leader
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
