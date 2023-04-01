72251e2fd8818a7b42ef97c38eb339f99e7b35fb
Troy Caro <iamtroy412@gmail.com>
2023-04-01 19:56:16 -0400

    fix: disable background, no need for color pencils

--- lua/troy/packer.lua
+++ lua/troy/packer.lua
@@ -13,9 +13,16 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  use({ 'rose-pine/neovim', as = 'rose-pine' })
-
-  vim.cmd('colorscheme rose-pine')
+  use({
+      'rose-pine/neovim',
+      as = 'rose-pine',
+      config = function()
+          require('rose-pine').setup({
+              disable_background = true,
+          })
+          vim.cmd('colorscheme rose-pine')
+      end
+  })
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
