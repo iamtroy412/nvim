859a727a30bead6b1a6a09cb728eed7a7e549d18
Troy Caro <iamtroy412@gmail.com>
2023-03-31 20:32:00 -0400

    ...wrong file

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,8 +3,8 @@ function ColorMyPencils(color)
 	vim.cmd.colorscheme(color)
 
 	-- Use for transparent background
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
